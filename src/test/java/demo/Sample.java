f865af347e5e73f30154f88df5e37c35a4605153
Bhavana S <gsbhavana123@gmail.com>
2025-03-28 09:42:48 +0530

    Second commit

---
+++ src/test/java/demo/Sample.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+
+	@Test
+	public void demo()
+	{
+		System.out.println("Hi");
+		System.out.println("workspace");
+	}
+}
